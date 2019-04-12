8ccadfbd8b14d9b4a1099e4e8d596c41f2a88378
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-04-12 14:20:30 -0300

    Move process pool to the inner _distribution_fit method

--- assignment1/code/assignment1/stats.py
+++ assignment1/code/assignment1/stats.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
 import functools
 import itertools
-from multiprocessing import Pool
+from multiprocessing import Pool as ProcessPool
+from multiprocessing.pool import ThreadPool
 import os
 import re
 import warnings
@@ -33,18 +34,21 @@ def np_cache(**kwargs):
 
         return wrapper
     return decorator
-#@functools.lru_cache(maxsize=16)
+
 @np_cache(maxsize=16)
 def distribution_fit(data):
     results = []
-    with Pool(processes=int(0.75*os.cpu_count()),
-            initializer=_warning_disabler) as p:
+    with ThreadPool(processes=int(0.25*os.cpu_count()),
+                    initializer=_warning_disabler) as p:
         results.extend(p.map(functools.partial(_distribution_fit, data), _distributions))
     return tuple(sorted((result for result in results if result is not None),
                         key=lambda dist: dist.fit))
 
 def _warning_disabler():
     warnings.simplefilter("ignore")
+
+def _kstest(data, distribution, arg):
+    return (stats.kstest(data, distribution, args=arg), arg)
 
 def _distribution_fit(data, distribution):
     fit = args = ()
@@ -55,12 +59,8 @@ def _distribution_fit(data, distribution):
         if 1 <= missing_arguments <= 2:
             available_args = (range(1,11),)*missing_arguments
 
-            best_fit = ()
-            for arg in itertools.product(*available_args):
-                fit = stats.kstest(data, distribution, args=arg)
-                if not best_fit or fit < best_fit:
-                    best_fit = fit
-                    args = arg
-            fit = best_fit
+            with ProcessPool(processes=int(0.5*os.cpu_count()),
+                             initializer=_warning_disabler) as p:
+                (fit, args) = min(p.map(functools.partial(_kstest, data, distribution), itertools.product(*available_args)))
 
     return DistributionFit(distribution, args, fit) if fit else None
