6b92e978c31ecc96b110d4338ac6c18831d61252
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-05-09 23:58:49 -0300

    Update assignment2 related material

---
+++ assignment2/README.txt
@@ -0,0 +1,3 @@
+# Instruções
+
+Abra o arquivo html encontrado na pasta code em seu navegador de preferência
---
+++ assignment2/report/code/example.py
@@ -0,0 +1,6 @@
+import antigravity
+import this
+
+print("Why hello there! Here you can find some Python code")
+
+print(f"Did you know that 1+1={1+1}?")
